417a10b18a7dcf3cebf3210ed6889a0c42603213
xh3b4sd <xh3b4sd@gmail.com>
2022-01-26 20:45:04 +0100

    update hack/push.sh (#1446)

--- hack/push.sh
+++ hack/push.sh
@@ -1,7 +1,7 @@
 i=${1}
 n=$(printf %07d $i)
 
-git checkout -b "philosophy/2021/${n}"
-git add "philosophy/2021/${n}"
-git commit -m "add philosophy/2021/${n}"
-git push -u origin "philosophy/2021/${n}"
+git checkout -b "philosophy/2022/${n}"
+git add "philosophy/2022/${n}"
+git commit -m "add philosophy/2022/${n}"
+git push -u origin "philosophy/2022/${n}"
