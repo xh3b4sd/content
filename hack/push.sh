f86f05e950bffeed4343698bc53ae6b5caa90273
xh3b4sd <xh3b4sd@gmail.com>
2024-09-21 11:49:21 +0200

    update hack (#1758)

--- hack/push.sh
+++ hack/push.sh
@@ -1,7 +1,7 @@
 i=${1}
 n=$(printf %07d $i)
 
-git checkout -b "philosophy/2023/${n}"
-git add "philosophy/2023/${n}"
-git commit -m "add philosophy/2023/${n}"
-git push -u origin "philosophy/2023/${n}"
+git checkout -b "philosophy/2024/${n}"
+git add "philosophy/2024/${n}"
+git commit -m "add philosophy/2024/${n}"
+git push -u origin "philosophy/2024/${n}"
